ee29a3ab75fa82b9faeb5c9190d598160495c5b4
sumeyyadede <dedesumeyya@gmail.com>
2020-07-06 15:28:38 -0400

    added freePasses field and added @Min and @Max annotations for validation.

--- spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.sumeyyadede.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -12,6 +14,10 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -27,5 +33,13 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}	
 	
 }

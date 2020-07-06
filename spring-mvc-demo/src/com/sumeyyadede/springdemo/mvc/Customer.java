82a70f566a48a0a270cacc631b412d43e8f143b1
sumeyyadede <dedesumeyya@gmail.com>
2020-07-06 15:02:41 -0400

    added message into @NotNull annotation.

--- spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
@@ -8,7 +8,7 @@ public class Customer {
 	private String firstName;
 	
 	// validation rules
-	@NotNull()
+	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
 

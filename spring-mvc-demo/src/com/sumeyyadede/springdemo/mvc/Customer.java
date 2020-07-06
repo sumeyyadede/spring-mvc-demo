3989e1a32548876deb0cee732e17dfb7e015fb81
sumeyyadede <dedesumeyya@gmail.com>
2020-07-06 15:45:11 -0400

    added NotNull annotation for freePasses.
    changed return type of the freePasses from int to Integer.
    
    
    added postalCode field.
    
    added regular expression pattern to the postalCode.

--- spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.sumeyyadede.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -14,9 +15,13 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -34,12 +39,20 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}	
 	
 }

edbf199adbc5591c1e0f1995c53ad22db81514c8
sumeyyadede <dedesumeyya@gmail.com>
2020-07-06 17:18:09 -0400

    added custom validation for courseCode.

--- spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/sumeyyadede/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.sumeyyadede.springdemo.mvc.validation.CourseCode;
+
 public class Customer {
 
 	private String firstName;
@@ -22,6 +24,9 @@ public class Customer {
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
+	
+	@CourseCode(value="TOPS", message="must start with TOPS")
+	private String courseCode;
 	
 	public String getFirstName() {
 		return firstName;
@@ -53,6 +58,14 @@ public class Customer {
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}	
 	
 }
